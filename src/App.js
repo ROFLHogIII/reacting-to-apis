1fc7c7760d23e958975946b4a1275d6057f3363f
Richard Garner <richardgarner@mac.com>
2018-09-05 13:52:58 -0500

    PHASE THREE complete

--- src/App.js
+++ src/App.js
@@ -2,43 +2,84 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import 'es6-promise';
 import MovieCard from "./components/MovieCard"
+import PeopleCard from "./components/PeopleCard"
 import logo from "./logo.png"
 
 
 const filmAPI = "https://ghibliapi.herokuapp.com/films/"
+const peopleAPI = "https://ghibliapi.herokuapp.com/people"
+
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             movies: [],
-            loadMovies: false
+            loadMovies: false,
+            people: [],
+            loadPeople: false
         }
     }
-    componentDidMount() {
+
+    loadMovies() {
+        this.setState({ loadMovies: true, loadPeople: false })
         fetch(filmAPI)
             .then(res => res.json())
             .then(ob => this.setState({ movies: ob }))
     }
-    loadMovies() {
-        this.setState({ loadMovies: true })
+    loadPeople() {
+        this.setState({ loadPeople: true, loadMovies: false })
+        fetch(peopleAPI)
+            .then(res => res.json())
+            .then(ob => this.setState({ people: ob }))
     }
+
     render() {
         if (this.state.loadMovies) {
             return (
                 <React.Fragment>
-                    {this.state.movies.map((movie, index) =>
-                        <MovieCard key={index} movie={movie} />
-                    )}
+                    <div className="row">
+                    <div className="col-md-12">
+                        <img className="rounded mx-auto d-block" src={logo} alt="Ghibli Logo" />
+                    </div>
+                    <div className="col-md-12">
+                        <button onClick={() => this.loadPeople()}>Load People</button>
+                    </div>
+                        {this.state.movies.map((movie, index) =>
+                            <MovieCard key={index} movie={movie} />
+                        )}
+                    </div>
                 </React.Fragment>
             )
         } else {
-            return (
-                <React.Fragment>
-                    <img src={logo} alt="Ghibli Logo" />
-                    <h1>Loading...</h1>
-                    <button onClick={() => this.loadMovies()}>Load Movies</button>
-                </React.Fragment>
-            )
+            if (this.state.loadPeople) {
+                return (
+                    <React.Fragment>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <img className="rounded mx-auto d-block" src={logo} alt="Ghibli Logo" />
+                            </div>
+                            <div className="col-md-12">
+                                <button onClick={() => this.loadMovies()}>Load Movies</button>
+                            </div>
+                            {this.state.people.map((person, index) =>
+                                <PeopleCard key={index} person={person} />
+                            )}
+                        </div>
+                    </React.Fragment>
+                )
+            } else {
+                return (
+                    <React.Fragment>
+                        <div className="col-md-12">
+                            <img className="rounded mx-auto d-block" src={logo} alt="Ghibli Logo" />
+                        
+                        <h1>Loading...</h1>
+                        <button onClick={() => this.loadMovies()}>Load Movies</button>
+                        <button onClick={() => this.loadPeople()}>Load People</button>
+                        </div>
+                    </React.Fragment>
+                )
+            }
         }
     }
 };
